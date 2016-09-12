36cb5e8e42975a0dc010b33bf367a13b685cee7d
renjiajian <renjiajian@withub.net.cn>
2016-09-12 22:20:06 +0800

    MessageActivity

--- app/src/main/java/com/unicorn/coordinate/profile/ProfileFragment.java
+++ app/src/main/java/com/unicorn/coordinate/profile/ProfileFragment.java
@@ -1,7 +1,13 @@
 package com.unicorn.coordinate.profile;
 
+import android.content.Intent;
+
 import com.unicorn.coordinate.R;
 import com.unicorn.coordinate.base.BaseFragment;
+import com.unicorn.coordinate.helper.ClickHelper;
+import com.unicorn.coordinate.message.MessageActivity;
+
+import butterknife.OnClick;
 
 
 public class ProfileFragment extends BaseFragment {
@@ -9,6 +15,14 @@ public class ProfileFragment extends BaseFragment {
     @Override
     public int getLayoutResId() {
         return R.layout.fragment_profile;
+    }
+
+    @OnClick(R.id.message)
+    public void messageOnClick() {
+        if (ClickHelper.isSafe()) {
+            Intent intent = new Intent(getActivity(), MessageActivity.class);
+            startActivity(intent);
+        }
     }
 
 }
