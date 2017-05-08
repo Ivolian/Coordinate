d782f0d1517a9ab4d21e56f108b29fda0a0bb223
renjiajian <renjiajian@withub.net.cn>
2017-05-08 13:45:12 +0800

    2、在设置里面，增加一个清楚缓存，清除本地信息； 有可能重复推送比赛，不清除怕数据混乱；

--- app/src/main/java/com/unicorn/coordinate/profile/setting/SettingActivity.java
+++ app/src/main/java/com/unicorn/coordinate/profile/setting/SettingActivity.java
@@ -1,12 +1,19 @@
 package com.unicorn.coordinate.profile.setting;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.unicorn.coordinate.R;
+import com.unicorn.coordinate.SimpleApplication;
 import com.unicorn.coordinate.base.BaseActivity;
 import com.unicorn.coordinate.helper.ClickHelper;
+import com.unicorn.coordinate.task.PointHelper;
+import com.unicorn.coordinate.task.model.TaskDao;
 import com.unicorn.coordinate.user.UpdatePasswordActivity;
 import com.unicorn.coordinate.utils.ConfigUtils;
+import com.unicorn.coordinate.utils.ToastUtils;
 
 import butterknife.OnClick;
 
@@ -36,6 +43,27 @@ public class SettingActivity extends BaseActivity {
         if (ClickHelper.isSafe()) {
             ConfigUtils.logout();
             finish();
+        }
+    }
+
+    @OnClick(R.id.clearCache)
+    public void clearCache() {
+        if (ClickHelper.isSafe()) {
+            new MaterialDialog.Builder(this)
+                    .title("清除缓存")
+                    .content("清除缓存可能导致当前比赛信息缺失，请勿在比赛前后及过程中清除缓存。您确定要清除缓存吗？")
+                    .positiveText("确认")
+                    .negativeText("取消")
+                    .onPositive(new MaterialDialog.SingleButtonCallback() {
+                        @Override
+                        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                            TaskDao taskDao = SimpleApplication.getInstance().getTaskDao();
+                            taskDao.deleteAll();
+                            PointHelper.deleteAll();
+                            ToastUtils.show("缓存已清除");
+                        }
+                    })
+                    .show();
         }
     }
 
