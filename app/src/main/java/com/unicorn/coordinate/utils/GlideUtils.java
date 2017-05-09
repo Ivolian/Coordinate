04516c3e6d83d83b77ec686e55b95287e9e320b3
renjiajian <renjiajian@withub.net.cn>
2017-05-09 23:50:04 +0800

    glide 增加加载图片方式

--- app/src/main/java/com/unicorn/coordinate/utils/GlideUtils.java
+++ app/src/main/java/com/unicorn/coordinate/utils/GlideUtils.java
@@ -6,12 +6,23 @@ import com.bumptech.glide.Glide;
 import com.unicorn.coordinate.R;
 import com.unicorn.coordinate.SimpleApplication;
 
+import java.io.File;
+
 
 public class GlideUtils {
 
     public static void loadPicture(String imgUrl, ImageView imageView) {
         Glide.with(SimpleApplication.getInstance())
                 .load(imgUrl)
+                .placeholder(R.drawable.placeholder)
+                .fitCenter()
+                .crossFade()
+                .into(imageView);
+    }
+
+    public static void loadPicture(File img, ImageView imageView) {
+        Glide.with(SimpleApplication.getInstance())
+                .load(img)
                 .placeholder(R.drawable.placeholder)
                 .fitCenter()
                 .crossFade()
